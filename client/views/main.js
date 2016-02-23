60719bc5f003ac7c9dcf54109cd5c2a77e95aac8
Damir Mustafin <frenchthebread@gmail.com>
2016-02-23 17:06:33 +0200

    Added sample dataset

--- client/views/main.js
+++ client/views/main.js
@@ -4,6 +4,22 @@ Template.home.rendered = function () {
     var svg2 = dimple.newSvg("#lineChartContainer", "100%", 400);
 
 
+    var initialData = [
+        {date: "2011-11-14T16:17:54Z", quantity: 2, total: 190, tip: 100, type: "tab"},
+        {date: "2011-11-15T16:20:19Z", quantity: 2, total: 190, tip: 100, type: "tab"},
+        {date: "2011-11-16T16:28:54Z", quantity: 1, total: 300, tip: 200, type: "visa"},
+        {date: "2011-11-17T16:30:43Z", quantity: 2, total: 90, tip: 0, type: "tab"},
+        {date: "2011-11-18T16:48:46Z", quantity: 2, total: 90, tip: 0, type: "tab"},
+        {date: "2011-11-19T16:53:41Z", quantity: 2, total: 90, tip: 0, type: "tab"},
+        {date: "2011-11-20T16:54:06Z", quantity: 1, total: 100, tip: 0, type: "cash"},
+        {date: "2011-11-21T16:58:03Z", quantity: 2, total: 90, tip: 0, type: "tab"},
+        {date: "2011-11-22T17:07:21Z", quantity: 2, total: 90, tip: 0, type: "tab"},
+        {date: "2011-11-23T17:22:59Z", quantity: 2, total: 90, tip: 0, type: "tab"},
+        {date: "2011-11-23T17:25:45Z", quantity: 2, total: 200, tip: 0, type: "cash"},
+        {date: "2011-11-25T17:29:52Z", quantity: 1, total: 200, tip: 100, type: "visa"}
+    ];
+
+
     var data = initialData;
     var dataForCrossFilter = initialData;
     var payments = crossfilter(dataForCrossFilter);
